078ed46c84ad756c3ca4a9659885af50dc4ad4e4
Alexander <abertholf7626@floridapoly.edu>
2022-11-02 13:21:40 -0400

    added statistics.h

---
+++ statistics.h
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdint.h>
+#include <stdlib.h>
+#include <string.h>
+
+#ifndef STATISTICS_H
+#define STATISTICS_H
+
+typedef struct {
+	char letter;
+	int frequency;
+} letter_frequency;
+
+size_t getNumChars(FILE*);
+char *getChars(FILE*, size_t);
+letter_frequency* do_frequency_analysis(char*, size_t, int, int);
+letter_frequency get_highest_frequency(letter_frequency*);
+
+#endif
